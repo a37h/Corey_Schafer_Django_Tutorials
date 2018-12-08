a019a92fbfbd0524b3704a280e7f5617bae08dae
Quasar <justfastmeow@gmail.com>
2018-12-08 03:33:57 +0300

    end of part 5

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Lalala',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 1, 2018'
-    },
-    {
-        'author': 'Lololo',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 2, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
-    return render(request, 'blog/home.html', {'posts': posts})
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
